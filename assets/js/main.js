38098e238e7f25b047564522e95ab4e50cbc1e8a
Houssam TAHRI <houssamtahri002@gmail.com>
2025-06-01 23:04:07 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,258 +1,273 @@
-/*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
-(function($) {
-
-	var	$window = $(window),
-		$body = $('body'),
-		$wrapper = $('#wrapper'),
-		$header = $('#header'),
-		$nav = $('#nav'),
-		$main = $('#main'),
-		$navPanelToggle, $navPanel, $navPanelInner;
-
-	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
-
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn._parallax = function(intensity) {
-
-		var	$window = $(window),
-			$this = $(this);
-
-		if (this.length == 0 || intensity === 0)
-			return $this;
-
-		if (this.length > 1) {
-
-			for (var i=0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
-
-			return $this;
-
-		}
-
-		if (!intensity)
-			intensity = 0.25;
-
-		$this.each(function() {
-
-			var $t = $(this),
-				$bg = $('<div class="bg"></div>').appendTo($t),
-				on, off;
-
-			on = function() {
-
-				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
-
-				$window
-					.on('scroll._parallax', function() {
-
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
-
-					});
-
-			};
-
-			off = function() {
-
-				$bg
-					.addClass('fixed')
-					.css('transform', 'none');
-
-				$window
-					.off('scroll._parallax');
-
-			};
-
-			// Disable parallax on ..
-				if (browser.name == 'ie'			// IE
-				||	browser.name == 'edge'			// Edge
-				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
-				||	browser.mobile)					// Mobile devices
-					off();
-
-			// Enable everywhere else.
-				else {
-
-					breakpoints.on('>large', on);
-					breakpoints.on('<=large', off);
-
-				}
-
-		});
-
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
-				$window.trigger('scroll');
-			});
-
-		return $(this);
-
-	};
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Scrolly.
-		$('.scrolly').scrolly();
-
-	// Background.
-		$wrapper._parallax(0.925);
-
-	// Nav Panel.
-
-		// Toggle.
-			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
-			)
-				.appendTo($wrapper);
-
-			// Change toggle styling once we've scrolled past the header.
-				$header.scrollex({
-					bottom: '5vh',
-					enter: function() {
-						$navPanelToggle.removeClass('alt');
-					},
-					leave: function() {
-						$navPanelToggle.addClass('alt');
-					}
-				});
-
-		// Panel.
-			$navPanel = $(
-				'<div id="navPanel">' +
-					'<nav>' +
-					'</nav>' +
-					'<a href="#navPanel" class="close"></a>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-navPanel-visible'
-				});
-
-			// Get inner.
-				$navPanelInner = $navPanel.children('nav');
-
-			// Move nav content on breakpoint change.
-				var $navContent = $nav.children();
-
-				breakpoints.on('>medium', function() {
-
-					// NavPanel -> Nav.
-						$navContent.appendTo($nav);
-
-					// Flip icon classes.
-						$nav.find('.icons, .icon')
-							.removeClass('alt');
-
-				});
-
-				breakpoints.on('<=medium', function() {
-
-					// Nav -> NavPanel.
-						$navContent.appendTo($navPanelInner);
-
-					// Flip icon classes.
-						$navPanelInner.find('.icons, .icon')
-							.addClass('alt');
-
-				});
-
-			// Hack: Disable transitions on WP.
-				if (browser.os == 'wp'
-				&&	browser.osVersion < 10)
-					$navPanel
-						.css('transition', 'none');
-
-	// Intro.
-		var $intro = $('#intro');
-
-		if ($intro.length > 0) {
-
-			// Hack: Fix flex min-height on IE.
-				if (browser.name == 'ie') {
-					$window.on('resize.ie-intro-fix', function() {
-
-						var h = $intro.height();
-
-						if (h > $window.height())
-							$intro.css('height', 'auto');
-						else
-							$intro.css('height', h);
-
-					}).trigger('resize.ie-intro-fix');
-				}
-
-			// Hide intro on scroll (> small).
-				breakpoints.on('>small', function() {
-
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'bottom',
-						top: '25vh',
-						bottom: '-50vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-				});
-
-			// Hide intro on scroll (<= small).
-				breakpoints.on('<=small', function() {
-
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'middle',
-						top: '15vh',
-						bottom: '-15vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-			});
-
-		}
-
-})(jQuery);
+$(function(){
+
+    $(window).on('load', function () {
+        $('.page-loader').delay('500').fadeOut(1000);
+    });
+
+    $(document).ready(function() {
+
+        $(document).on('click', '.icon-menu', function() {
+            $('.responsive-sidebar-menu').addClass('active');
+        });
+        $(document).on('click', '.responsive-sidebar-menu .overlay', function() {
+            $('.responsive-sidebar-menu').removeClass('active');
+        });
+
+        $(document).on('click', '.menu li .scroll-to', function() {
+            $('.responsive-sidebar-menu').removeClass('active');
+        })
+
+
+        $(document).on('click', ".color-boxed a", function() {
+            $(".color-boxed a").removeClass("clr-active");
+            $(this).addClass("clr-active");
+        });
+        
+        $(document).on('click', ".global-color .setting-toggle", function() {
+            $(".global-color").addClass("active");
+        });
+
+        $(document).on('click', ".global-color .inner .overlay, .global-color .inner .global-color-option .close-settings", function() {
+            $(".global-color").removeClass("active");
+        });
+
+    });
+
+    $(window).scroll(function() {
+            
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 0) {
+            $('.page-section').each(function(i) {
+                if ($(this).position().top <= windscroll - -1) {
+                    $('.scroll-nav .scroll-to.active').removeClass('active');
+                    $('.scroll-nav .scroll-to').eq(i).addClass('active');
+                    $('.scroll-nav-responsive a.active').removeClass('active');
+                    $('.scroll-nav-responsive a').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+
+            $('.scroll-nav .scroll-to.active').removeClass('active');
+            $('.scroll-nav .scroll-to:first').addClass('active');
+            $('.scroll-nav-responsive a.active').removeClass('active');
+            $('.scroll-nav-responsive a:first').addClass('active');
+        }
+
+        if (windscroll >= 0) {
+            $('.scroll-to-page').each(function(i) {
+
+                var wscrolldecress = windscroll + 1;
+                // console.log(wscrolldecress);
+                if ($(this).position().top <= wscrolldecress - 0) {
+                    $('.scroll-nav .scroll-to.active').removeClass('active');
+                    $('.scroll-nav .scroll-to').eq(i).addClass('active');
+                    $('.scroll-nav-responsive a.active').removeClass('active');
+                    $('.scroll-nav-responsive a').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+            $('.scroll-nav .scroll-to.active').removeClass('active');
+            $('.scroll-nav .scroll-to:first').addClass('active');
+            $('.scroll-nav-responsive a.active').removeClass('active');
+            $('.scroll-nav-responsive a:first').addClass('active');
+        }
+
+    }).scroll();
+
+
+
+
+
+
+
+    if ($('.testimonial-slider').length) {
+        var testimonial = $('.testimonial-slider').owlCarousel({
+            items: 1,
+            margin: 30,
+            stagePadding: 0,
+            smartSpeed: 450,
+            autoHeight: true,
+            loop: false,
+            nav: false,
+            dots: false,
+            onInitialized  : counter, //When the plugin has initialized.
+            onTranslated : counter //When the translation of the stage has finished.
+        });
+
+        $('.testimonial-nav .next').on('click', function() {
+            testimonial.trigger('next.owl.carousel');
+        })
+        $('.testimonial-nav .prev').on('click', function() {
+            testimonial.trigger('prev.owl.carousel', [300]);
+        })
+
+
+        function counter(event) {
+            var element   = event.target;         // DOM element, in this example .owl-carousel
+            var items     = event.item.count;     // Number of items
+            var item      = event.item.index + 1;     // Position of the current item
+        
+        // it loop is true then reset counter from 1
+        if(item > items) {
+                item = item - items
+        }
+        $('#testimonial-slide-count').html("<span class='left'>"+item+"</span> / "+items)
+        }
+    }
+
+    // function remove_is_active() {
+    //     $(".menu .scroll-to").removeClass("active");
+    // }
+
+    // gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+    // var container = document.querySelector("#smooth-content");
+
+    // var height;
+    // function setHeight() {
+    //     height = container.clientHeight;
+
+
+    //     document.body.style.height = height + "px";
+    // }
+    // ScrollTrigger.addEventListener("refreshInit", setHeight);
+
+    // gsap.to(container, {
+    //     y: () => -(height - document.documentElement.clientHeight),
+    //     ease: "none",
+    //     scrollTrigger: {
+    //         trigger: container,
+    //         start: "top top",
+    //         end: "bottom bottom",
+    //         scrub: 1,
+    //         invalidateOnRefresh: true,
+    //     }
+    // });
+
+    window.addEventListener('scroll', {
+        scroll_animations,
+    });
+
+
+    // Array.prototype.slice.call(document.querySelectorAll(".page-section")).forEach(function (e, t) {
+    //     ScrollTrigger.create({
+    //         trigger: e,
+    //         id: t + 1,
+    //         start: "top center",
+    //         end: function () {
+    //             return "+=".concat(e.clientHeight - 30);
+    //         },
+    //         toggleActions: "play reverse none reverse",
+    //         toggleClass: { targets: e, className: "active" },
+    //         onToggle: function () {
+    //             $(".menu .scroll-to").removeClass("active"), "" != e.id && $('.menu .scroll-to[href*="#' + e.id + '"]').addClass("active");
+    //         },
+    //     });
+    // });
+
+    // document.querySelectorAll('.scroll-to').forEach((e) => {
+    //     const target = e.getAttribute('href');
+    //     const targetEl = document.querySelector(target);
+    //     // const targetRect = targetEl.getBoundingClientRect();
+
+
+    //     var offset = gsap.getProperty("#smooth-content", "y");
+    //     var position = jQuery(target).get(0).getBoundingClientRect().top - offset;
+    
+
+    //     e.addEventListener('click', (e) => {
+    //         e.preventDefault();
+
+    //         gsap.to(window, {
+    //             scrollTo: position,
+    //             ease: "power4",
+    //             duration: 0.1,
+    //             onToggle: function () {
+    //                 console.log('toggle');
+    //                 remove_is_active();
+    //                 if (targetEl.id != "") $('.menu .scroll-to[href*="#' + targetEl.id + '"]').addClass("active");
+    //             },
+    //             onLeaveBack: function () {
+    //                 console.log('leave back');
+    //                 remove_is_active();
+    //                 if (targetEl.id != "") $('.menu .scroll-to[href*="#' + targetEl.id + '"]').addClass("active");
+    //             },
+    //             onLeave: function () {
+    //                 console.log('leave');
+    //                 remove_is_active();
+    //                 if (targetEl.id != "") $('.menu .scroll-to[href*="#' + targetEl.id + '"]').addClass("active");
+    //             },
+    //             overwrite: !0,
+    //         });
+    //     });
+
+        
+    
+    // });
+
+});
+
+
+
+function scroll_animations() {
+    // var allow_on_mobile = !0;
+    // if (typeof config_scroll_animation_on_mobile !== "undefined") allow_on_mobile = config_scroll_animation_on_mobile;
+    // if (allow_on_mobile == !1 && is_mobile_device) return;
+    var defaults = {
+        duration: 1.2,
+        ease: "power4.out",
+        animation: "fade_from_bottom",
+        once: !1,
+    };
+    gsap.utils.toArray(".scroll-animation").forEach(function (box) {
+        var gsap_obj = {};
+        var settings = {
+            // ease: box.dataset.animationEase || defaults.ease,
+            duration: box.dataset.animationDuration || defaults.duration,
+        };
+        var animations = {
+            fade_from_bottom: {
+                y: 180,
+                opacity: 0,
+            },
+            fade_from_top: {
+                y: -180,
+                opacity: 0,
+            },
+            fade_from_left: {
+                x: -180,
+                opacity: 0,
+            },
+            fade_from_right: {
+                x: 180,
+                opacity: 0,
+            },
+            fade_in: {
+                opacity: 0,
+            },
+            rotate_up: {
+                y: 180,
+                rotation: 10,
+                opacity: 0,
+            },
+        };
+        var scroll_trigger = {
+            scrollTrigger: {
+                trigger: box,
+                once: defaults.once,
+                start: "top bottom+=20%",
+                // start: "top bottom+=5%",
+                toggleActions: "play none none reverse",
+                markers: !1,
+            },
+        };
+        jQuery.extend(gsap_obj, settings);
+        jQuery.extend(gsap_obj, animations[box.dataset.animation || defaults.animation]);
+        jQuery.extend(gsap_obj, scroll_trigger);
+        gsap.from(box, gsap_obj);
+    });
+}
+scroll_animations();
